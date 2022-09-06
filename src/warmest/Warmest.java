f5139f83f6b41d5d7471ac9b02d22c9d5c6fe8b1
Abigail <abigaildavidov@gmail.com>
2022-09-06 11:32:08 +0300

    change in remove method

--- src/warmest/Warmest.java
+++ src/warmest/Warmest.java
@@ -1,21 +1,22 @@
 package warmest;
 
 import java.util.Hashtable;
+import java.util.Stack;
 
 public class Warmest<K,V> {
 	
 	private Hashtable<K,V> map = new Hashtable<K,V>();
-	private V warmest;
+	private Stack<V> warmest = new Stack<V>();
 	
 	public void put(K k, V v) { 
-		warmest = v;
+		warmest.push(v);
 		map.put(k,v);
 	}
 	
 	public V get(K k) {
 		if(map.containsKey(k)) {
-			warmest = map.get(k);
-			return warmest;
+			warmest.push(map.get(k));
+			return map.get(k);
 		}
 		else {
 			return null;
@@ -25,8 +26,8 @@ public class Warmest<K,V> {
 	
 	public V remove(K k) {
 		if(map.containsKey(k)) {
-			if(map.get(k) == warmest && !map.isEmpty())
-				warmest = map.values().iterator().next();
+			if(map.get(k) == warmest.peek())
+				warmest.pop();
 			return map.remove(k);		
 		}
 		else 
@@ -37,8 +38,7 @@ public class Warmest<K,V> {
 		if(map.isEmpty()) 
 			return null;
 		else
-			return warmest;
+			return warmest.peek();
 	}
 	
-
 }
